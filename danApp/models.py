7940d9a80c71d824d999f9eec1c9259cc10e7614
da-nn-yy <db7394928@gmail.com>
2024-07-31 14:43:08 +0200

    modified

--- danApp/models.py
+++ danApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class DanApp(models.Model):
+  origin_country = models.CharField(max_length=64)
+  destination_country = models.CharField(max_length=64)
+  nights = models.models.models.IntegerField()
+  price = models.models.models.IntegerField()
+  
+cd 
