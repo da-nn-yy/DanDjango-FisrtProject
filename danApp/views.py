28af358c432900b6a3140d441f7b5b08b68db1de
da-nn-yy <db7394928@gmail.com>
2024-08-01 11:31:57 +0200

    added a form.py and modified views.py

--- danApp/views.py
+++ danApp/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import ContactForm
 from .models import DanApp
 # from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-  # return HttpResponse("Hello, world. You're at the danApp index."
-  tours = DanApp.objects.all()
-  context = {'tours' : tours}
-  return render(request, 'danApp/index.html',context) 
+# def index(request):
+#   # return HttpResponse("Hello, world. You're at the danApp index."
+#   tours = DanApp.objects.all()
+#   context = {'tours' : tours}
+#   return render(request, 'danApp/index.html',context) 
+
+
+#Home page view func.
+def home_view(request):
+  return render(request, 'danApp/home.html',)
+
+def contact_view(request):
+  if request.method == 'POST':
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      form.send_email()
+      return redirect('success')
+  else:
+    form = ContactForm()
+  context = {'form' : form}
+  return render(request,'danApp/contact.html')
+
+def contact_success(request):
+  return(request,'danApp/success.html')
