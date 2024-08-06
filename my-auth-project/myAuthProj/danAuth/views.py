974df3737bdc32d3fc3ee79207164909130cf84b
da-nn-yy <db7394928@gmail.com>
2024-08-06 11:35:53 +0200

    modified a viiew.py with adding videw functions

--- my-auth-project/myAuthProj/danAuth/views.py
+++ my-auth-project/myAuthProj/danAuth/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate , login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.view import View
+from django.contrib.auth.models import User
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+  if request.method == "POST":
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get("username")
+      password = form.cleaned_data.get("password")
+      user = User.objects.create_user(username=username,password=password) 
+      login(request,user)
+      return redirect('home')
+    else:
+      form = RegisterForm()
+      return render(request, 'accounts/regist.html',{'form':form})
+
+def login_view(request):
+  pass
+def logout_view(request):
+  pass
+
+#home page using the decorator
+
+def home_view(request):
+  pass
