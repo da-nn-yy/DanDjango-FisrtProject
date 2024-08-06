9f351bb2c683f4f71d93524063da42ec9cd62495
da-nn-yy <db7394928@gmail.com>
2024-08-06 11:42:35 +0200

    added a git ignore file

--- my-auth-project/myAuthProj/myAuthProj/settings.py
+++ my-auth-project/myAuthProj/myAuthProj/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!h@n=)joc3azl7wz^2fvtlfsdw-$47bsp=7)9w&x2zdm+!f_!'
+
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
